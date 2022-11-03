619728ff1aedc97d613cb3c8e59083361168b73b
Brandon Chan <bchan26@bu.edu>
2022-11-02 20:38:32 -0400

    updated readme, created .env global.js and .gitignore

---
+++ mbta-tracker/src/global.js
@@ -0,0 +1,5 @@
+export const BASE_URL = 'https://api-v3.mbta.com'
+
+export const INFO_PATH = 'https://api-v3.mbta.com/stops/'
+
+export const TIME_PATH = 'https://api-v3.mbta.com/predictions?filter[stop]='

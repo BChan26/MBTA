53bbb8a6d003956fccf63865d1f2a4deee9fc9c0
Brandon Chan <bchan26@bu.edu>
2022-11-09 10:43:24 -0500

    updated the readme and added id values to each of the dropdown options in the form to resolve an error

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ I will be making an API/Axios call to the MBTA (Massachusetts Bay Transportation
 [65bus.netlify.app](https://65bus.netlify.app/ "Link to 65 Bus Tracker Website")
 ![image](https://i.postimg.cc/jq3nPDbk/65-Bus-Tracker-Page.png)
 
-### Final Product
+## Final Product
 My goal was to create a tracker for the 65 bus, a route I use to go to the local library. 
 
 I have shortcuts to 2 specific stops I use the most:
@@ -14,18 +14,18 @@ I have shortcuts to 2 specific stops I use the most:
 
 I also have a component that allows you to pick any bus stop along the 65 and see how many minutes away the next bus is.
 
-#### Big Takeaways/Challenges
+## Big Takeaways/Challenges
 - This project forced me to review the fundamentals of React and Hooks to truly understand how each component and prop interacts.
 - Each API out there has data structured differently. Compared to my first Weather Forecast project, the MBTA's API had more data to work with, but it was less intuitiveness to navigate (AKA it was very dense info to parse out)
 - Changing the API's military 24-hour time into "X Minutes Away" being displayed required deconstruction, math between strings and numbers (parseInt), and pseudo-coding out the logic to implement this.
 
-#### Extensions
+## Extensions
 With more time, I'd love to:
 - Make this scaleable to all bus/train lines in the MBTA
 - Sync an AXIOS call's value into another mapped out Axios call to display the intersection name instead of a stop ID (which isn't intuitive)
 - Connect each line to a live map with the stops, for better user-interactivity
 
-#### 3rd Party Resources Used
+## 3rd Party Resources Used
 - Coding Languages/Libraries Needed: HTML, CSS, Javascript, and React (18.2)
 - API: https://api-v3.mbta.com
 - Image Editor: https://pixlr.com/
@@ -34,11 +34,11 @@ With more time, I'd love to:
 - W3Schools React Tutorial: https://www.w3schools.com/react/ 
 - Getting Current Time: https://reactgo.com/javascript-get-time/ 
 
-#### Installation and Feedback
+## Installation and Feedback
 - After forking on Github, you can git clone "URL" in your terminal (with the URL from the search bar)
 - You will need to npm install react-router-dom and npm install axios for this to work properly 
 - If you're interested in helping me with my extenions, feel free to reach out at bchan26@bu.edu
 
-#### Credits
+## Credits
 - The Teaching Team (Jeremy, Brittany, Tiffany, Kristina, Tylus)
 - Robert & Brittany (for helping me save a value in a UseState hook to make a dynamic axios call)
--- mbta-tracker/src/components/The65.jsx
+++ mbta-tracker/src/components/The65.jsx
@@ -21,64 +21,64 @@ export default function The65 ({formState, handleChange, handleSubmit, newValue,
                 
                 <option value=""></option>
                 {/* Inbound */}
-                <option value="1026">1.  (Inbound) Chestnut Hill Ave @ Veronica Smith Ctr</option>
-                <option value="919"> 2.  (Inbound) Washington St opp Waldo Terrace</option>
-                <option value="1268">3.  (Inbound) Washington St @ Sheperd St</option>
-                <option value="1269">4.  (Inbound) Washington St @ Snow St</option>
-                <option value="1270">5.  (Inbound) Washington St @ Monastery Rd</option>
-                <option value="1272">6.  (Inbound) Washington St @ Euston St</option>
-                <option value="1273">7.  (Inbound) Washington St @ Comm Ave</option>
-                <option value="1274">8.  (Inbound) Washington St @ Corey Rd</option>
-                <option value="1275">9.  (Inbound) Washington St @ Evans Rd</option>
-                <option value="1276">10. (Inbound) Washington St @ Beacon St</option>
-                <option value="1277">11. (Inbound) 684 Washington St </option>
-                <option value="1278">12. (Inbound) Washington St @ Winthrop Path</option>
-                <option value="1279">13. (Inbound) Washington St @ Gardner Rd</option>
-                <option value="1280">14. (Inbound) Washington St @ Gardner Path</option>
-                <option value="1281">15. (Inbound) Washington St @ Greenough Circle</option>
-                <option value="1282">16. (Inbound) Washington St @ Cypress St</option>
-                <option value="1283">17. (Inbound) Washington St @ Harvard St</option>
-                <option value="1555">18. (Inbound) Washington St @ Walnut St</option>
-                <option value="1556">19. (Inbound) 678 Brookline Ave opp Pearl St</option>
-                <option value="1557">20. (Inbound) Brookline Ave opp Aspinwall Ave</option>
-                <option value="1558">21. (Inbound) Brookline Ave @ Francis St</option>
-                <option value="1559">22. (Inbound) Brookline Ave @ Deaconess Rd</option>
-                <option value="1804">23. (Inbound) Brookline Ave @ Longwood Ave</option>
-                <option value="1805">24. (Inbound) Brookline Ave opp Short St</option>
-                <option value="1806">25. (Inbound) Brookline Ave @ The Fenway</option>
-                <option value="9441">26. (Inbound) Brookline Ave @ Boylston St</option>
-                <option value="1562">27. (Inbound) 132 Brookline Ave opp Fullerton St</option>
-                <option value="1563">28. (Inbound) Brookline Ave @ Jersey St</option>
-                <option value="1564">29. (Inbound) Brookline Ave @ Newbury St</option>
+                <option id="1026" value="1026">1.  (Inbound) Chestnut Hill Ave @ Veronica Smith Ctr</option>
+                <option id="919" value="919"> 2.  (Inbound) Washington St opp Waldo Terrace</option>
+                <option id="1268" value="1268">3.  (Inbound) Washington St @ Sheperd St</option>
+                <option id="1269" value="1269">4.  (Inbound) Washington St @ Snow St</option>
+                <option id="1270" value="1270">5.  (Inbound) Washington St @ Monastery Rd</option>
+                <option id="1272" value="1272">6.  (Inbound) Washington St @ Euston St</option>
+                <option id="1273" value="1273">7.  (Inbound) Washington St @ Comm Ave</option>
+                <option id="1274" value="1274">8.  (Inbound) Washington St @ Corey Rd</option>
+                <option id="1275" value="1275">9.  (Inbound) Washington St @ Evans Rd</option>
+                <option id="1276" value="1276">10. (Inbound) Washington St @ Beacon St</option>
+                <option id="1277" value="1277">11. (Inbound) 684 Washington St </option>
+                <option id="1278" value="1278">12. (Inbound) Washington St @ Winthrop Path</option>
+                <option id="1279" value="1279">13. (Inbound) Washington St @ Gardner Rd</option>
+                <option id="1280" value="1280">14. (Inbound) Washington St @ Gardner Path</option>
+                <option id="1281" value="1281">15. (Inbound) Washington St @ Greenough Circle</option>
+                <option id="1282" value="1282">16. (Inbound) Washington St @ Cypress St</option>
+                <option id="1283" value="1283">17. (Inbound) Washington St @ Harvard St</option>
+                <option id="1555" value="1555">18. (Inbound) Washington St @ Walnut St</option>
+                <option id="1556" value="1556">19. (Inbound) 678 Brookline Ave opp Pearl St</option>
+                <option id="1557" value="1557">20. (Inbound) Brookline Ave opp Aspinwall Ave</option>
+                <option id="1558" value="1558">21. (Inbound) Brookline Ave @ Francis St</option>
+                <option id="1559" value="1559">22. (Inbound) Brookline Ave @ Deaconess Rd</option>
+                <option id="1804" value="1804">23. (Inbound) Brookline Ave @ Longwood Ave</option>
+                <option id="1805" value="1805">24. (Inbound) Brookline Ave opp Short St</option>
+                <option id="1806" value="1806">25. (Inbound) Brookline Ave @ The Fenway</option>
+                <option id="9441" value="9441">26. (Inbound) Brookline Ave @ Boylston St</option>
+                <option id="1562" value="1562">27. (Inbound) 132 Brookline Ave opp Fullerton St</option>
+                <option id="1563" value="1563">28. (Inbound) Brookline Ave @ Jersey St</option>
+                <option id="1564" value="1564">29. (Inbound) Brookline Ave @ Newbury St</option>
                 {/* Outbound */}
-                <option value="899"> 1.  (Outbound) Kenmore Station</option>
-                <option value="8993">2.  (Outbound) Brookline Ave opp Newbury St</option>
-                <option value="1518">3.  (Outbound) Brookline Ave opp Jersey St</option>
-                <option value="1519">4.  (Outbound) Brookline Ave @ Fullerton St</option>
-                <option value="1520">5.  (Outbound) Brookline Ave @ Park Dr</option>
-                <option value="1777">6.  (Outbound) Brookline Ave @ Pilgrim Rd</option>
-                <option value="1778">7.  (Outbound) Brookline Ave @ Short St</option>
-                <option value="1521">8.  (Outbound) Brookline Ave @ Longwood Ave</option>
-                <option value="1523">9.  (Outbound) Brookline Ave @ Francis St</option>
-                <option value="1524">10. (Outbound) Brookline Ave @ Aspinwall Ave</option>
-                <option value="1525">11. (Outbound) Brookline Ave @ Pearl St</option>
-                <option value="1526">12. (Outbound) Washington St @ Pearl St</option>
-                <option value="1286">13. (Outbound) Washington St @ Harvard St</option>
-                <option value="1287">14. (Outbound) Washington St @ School St</option>
-                <option value="1288">15. (Outbound) Washington St opp Greenough St</option>
-                <option value="1289">16. (Outbound) Washington St @ Griggs St</option>
-                <option value="1290">17. (Outbound) Washington St opp Gardner Rd</option>
-                <option value="1291">18. (Outbound) Washington St @ Fairbanks St</option>
-                <option value="1292">19. (Outbound) Washington St @ Beacon St</option>
-                <option value="1293">20. (Outbound) Washington St @ Bartlett St</option>
-                <option value="1294">21. (Outbound) Washington St @ Corey Rd</option>
-                <option value="1295">22. (Outbound) Washington St @ Comm Ave</option>
-                <option value="1296">23. (Outbound) Washington St opp Euston Rd</option>
-                <option value="1298">24. (Outbound) Washington St @ Monastery Rd</option>
-                <option value="1299">25. (Outbound) Washington St opp Snow St</option>
-                <option value="1300">26. (Outbound) Washington St opp Shepard St</option>
-                <option value="1301">27. (Outbound) 7 Winship St</option>
-                <option value="1994">28. (Outbound) Winship St @ Union St</option>
+                <option id="899" value="899"> 1.  (Outbound) Kenmore Station</option>
+                <option id="8993" value="8993">2.  (Outbound) Brookline Ave opp Newbury St</option>
+                <option id="1518" value="1518">3.  (Outbound) Brookline Ave opp Jersey St</option>
+                <option id="1519" value="1519">4.  (Outbound) Brookline Ave @ Fullerton St</option>
+                <option id="1520" value="1520">5.  (Outbound) Brookline Ave @ Park Dr</option>
+                <option id="1777" value="1777">6.  (Outbound) Brookline Ave @ Pilgrim Rd</option>
+                <option id="1778" value="1778">7.  (Outbound) Brookline Ave @ Short St</option>
+                <option id="1521" value="1521">8.  (Outbound) Brookline Ave @ Longwood Ave</option>
+                <option id="1523" value="1523">9.  (Outbound) Brookline Ave @ Francis St</option>
+                <option id="1524" value="1524">10. (Outbound) Brookline Ave @ Aspinwall Ave</option>
+                <option id="1525" value="1525">11. (Outbound) Brookline Ave @ Pearl St</option>
+                <option id="1526" value="1526">12. (Outbound) Washington St @ Pearl St</option>
+                <option id="1286" value="1286">13. (Outbound) Washington St @ Harvard St</option>
+                <option id="1287" value="1287">14. (Outbound) Washington St @ School St</option>
+                <option id="1288" value="1288">15. (Outbound) Washington St opp Greenough St</option>
+                <option id="1289" value="1289">16. (Outbound) Washington St @ Griggs St</option>
+                <option id="1290" value="1290">17. (Outbound) Washington St opp Gardner Rd</option>
+                <option id="1291" value="1291">18. (Outbound) Washington St @ Fairbanks St</option>
+                <option id="1292" value="1292">19. (Outbound) Washington St @ Beacon St</option>
+                <option id="1293" value="1293">20. (Outbound) Washington St @ Bartlett St</option>
+                <option id="1294" value="1294">21. (Outbound) Washington St @ Corey Rd</option>
+                <option id="1295" value="1295">22. (Outbound) Washington St @ Comm Ave</option>
+                <option id="1296" value="1296">23. (Outbound) Washington St opp Euston Rd</option>
+                <option id="1298" value="1298">24. (Outbound) Washington St @ Monastery Rd</option>
+                <option id="1299" value="1299">25. (Outbound) Washington St opp Snow St</option>
+                <option id="1300" value="1300">26. (Outbound) Washington St opp Shepard St</option>
+                <option id="1301" value="1301">27. (Outbound) 7 Winship St</option>
+                <option id="1994" value="1994">28. (Outbound) Winship St @ Union St</option>
                 </select>
         
                 {/* Upon the button onclick, need event.preventDefault() to prevent refreshing of dropdown, setting a new value with value above, running the handlesubmit, and running the Axios call with getSchedule with the new value */}
